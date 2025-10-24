a23996daf7607f2262698e27d2e2d2a68608f230
rajeev <rajivg216@gmail.com>
2025-10-24 15:28:19 +0530

    GoLang : Variables, Constants and Types Example.

--- 01Hello/main.go
+++ 01Hello/main.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello from golang , rajeev....");
+
+	// Variables , Constants and Types Example
+	var name string = "Rajeev";    // Variable declarations method
+	city := "Navi Mumbai";   // Shorthand method for variable declarations
+	const pi = 3.143;
+
+	fmt.Printf("Hello %s from %s, the value of pi is : %.2f\n ", name, city, pi);
+
 }
